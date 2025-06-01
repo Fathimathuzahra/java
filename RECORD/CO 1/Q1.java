900d66286180d2ca5f0f74b6679214f83f8698bd
Fathimathuzahra <fz551914@gmail.com>
2025-06-01 19:46:55 +0530

    Update Q1.java

--- RECORD/CO 1/Q1.java
+++ RECORD/CO 1/Q1.java
@@ -43,13 +43,3 @@ Enter product code, name and price : 1025 item_B 2200
 Enter product code, name and price : 1026 item_C 3000
 item_A has the lowest price : 2000
 
-=== Code Execution Successful ===
-
-
-
-Algorithm: Lowest Price Product
-Step 1: Define class Product with variables: int p_code, String p_name, int price.
-Step 2: In constructor, read p_code, p_name, and price using Scanner.
-Step 3: Create three objects: p1, p2, p3.
-Step 4: Compare p1.price, p2.price, and p3.price.
-Step 5: Display p_name and price of the product with the lowest value.
